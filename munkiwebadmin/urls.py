df6450767be40a31e7e0e9bc545ebf0f83b0dd64
Rod Christiansen <rod@focused.systems>
2025-08-26 23:11:03 -0700

    Update Django URL patterns for modern Django compatibility
    - Replace deprecated django.conf.urls.url with re_path
    - Update auth views imports for Django 3+
    - Fix print statements for Python 3
    - Temporarily disable admin due to Django package issues
    - Maintain URL functionality while supporting newer Django versions

--- munkiwebadmin/urls.py
+++ munkiwebadmin/urls.py
@@ -1,26 +1,25 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import django.contrib.auth.views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.core.exceptions import ImproperlyConfigured
 
-from django.contrib import admin
-admin.autodiscover()
-
+# Temporarily comment out admin to avoid corrupted Django package
+# from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # re_path(r'^admin/', admin.site.urls),
 
-    url(r'^login/$', django.contrib.auth.views.login, name='login'),
-    url(r'^logout/$', django.contrib.auth.views.logout_then_login,
-        name='logout'),
-    url(r'^api/', include('api.urls')),
-    url(r'^manifests/', include('manifests.urls')),
-    url(r'^catalogs/', include('catalogs.urls')),
-    url(r'^pkgsinfo/', include('pkgsinfo.urls')),
-    url(r'^makecatalogs/', include('process.urls')),
-    url(r'^$', RedirectView.as_view(url='/manifests/', permanent=False), name='manifests'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    re_path(r'^api/', include('api.urls')),
+    re_path(r'^manifests/', include('manifests.urls')),
+    re_path(r'^catalogs/', include('catalogs.urls')),
+    re_path(r'^pkgsinfo/', include('pkgsinfo.urls')),
+    re_path(r'^makecatalogs/', include('process.urls')),
+    re_path(r'^$', RedirectView.as_view(url='/manifests/', permanent=False), name='manifests'),
 ]
 # comment out the following if you are serving
 # static files a different way
@@ -29,7 +28,7 @@ urlpatterns += staticfiles_urlpatterns()
 # debug/development serving MEDIA files (icons)
 try:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-except django.core.exceptions.ImproperlyConfigured:
-    print "**** MEDIA_URL or MEDIA_ROOT missing from settings.py       ****"
-    print "**** copy MEDIA_URL or MEDIA_ROOT from settings_template.py ****"
+except ImproperlyConfigured:
+    print("**** MEDIA_URL or MEDIA_ROOT missing from settings.py       ****")
+    print("**** copy MEDIA_URL or MEDIA_ROOT from settings_template.py ****")
     raise
